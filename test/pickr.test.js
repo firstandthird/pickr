3046a149dfebf9cf7d3370debd23e66b0c847dd7
Troy Whiteley <troy@somanyscientists.com>
2013-11-11 23:34:03 -0500

    WIP - Beginnings of lib.

--- test/pickr.test.js
+++ test/pickr.test.js
@@ -1,4 +1,49 @@
-
 suite('pickr', function() {
+  teardown(function(){
+    $('.pickr-modal').remove();
+    var el = $('#date-picker');
+    el.unbind();
+    el.removeData();
+    el.blur();
+  });
 
+  suite('init', function() {
+    test('pickr exists', function() {
+      assert.ok(typeof $.fn.pickr === 'function');
+    });
+
+    test('pickr class on input', function() {
+      var el = $('#date-picker');
+      
+      el.pickr();
+
+      assert.ok(el.hasClass('pickr'));
+    });
+  });
+
+  suite('show/hide', function() {
+    test('pickr should show on focus', function() {
+      var el = $('#date-picker');
+
+      el.pickr();
+
+      el.focus();
+
+      assert.equal($('.pickr-modal').length, 1);
+    });
+
+    test('pickr should hide on blur', function() {
+      var el = $('#date-picker');
+
+      el.pickr();
+
+      el.focus();
+
+      assert.equal($('.pickr-modal').length, 1);
+
+      el.blur();
+
+      assert.equal($('.pickr-modal').length, 0);
+    });
+  });
 });
